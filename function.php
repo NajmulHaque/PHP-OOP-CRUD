ee30fc77992c6e4b8c9d864f0396ed52aff4af79
NajmulHoq <najmul@shuttlebd.com>
2020-10-26 00:40:58 +0600

    initial commit

--- function.php
+++ function.php
@@ -3,7 +3,7 @@ session_start();
 define('DB_SERVER','localhost');
 define('DB_USER','root');
 define('DB_PASS' ,'');
-define('DB_NAME', 'oopscrud');
+define('DB_NAME', 'database');
 class DB_con
 {
 function __construct()
@@ -21,14 +21,14 @@ echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	public function insert($name,$father,$mother,$email,$phone,$address,$entry_date)
 	{
 	// $ret=mysqli_query($this->dbh,"insert into tblusers(FirstName,LastName,EmailId,ContactNumber,Address) values('$fname','$lname','$emailid','$contactno','$address')");
-		$ret=mysqli_query($this->dbh,"insert into customer(name,father,mother,email,phone,address,entry_date) values('$name','$father','$mother','$email','$phone','$address','$entry_date')");
+		$ret=mysqli_query($this->dbh,"insert into employee(name,father,mother,email,phone,address,entry_date) values('$name','$father','$mother','$email','$phone','$address','$entry_date')");
 	return $ret;
 	}
 
 //Data read Function
 public function fetchdata()
 	{
-	$result=mysqli_query($this->dbh,"select * from customer") or die( mysqli_error($this->dbh));
+	$result=mysqli_query($this->dbh,"select * from employee") or die( mysqli_error($this->dbh));
 	return $result;
 	}
 
@@ -36,7 +36,7 @@ public function fetchdata()
 //Data one record read Function
 public function fetchonerecord($id)
 	{
-	$oneresult=mysqli_query($this->dbh,"select * from customer where id=$id");
+	$oneresult=mysqli_query($this->dbh,"select * from employee where id=$id");
 	return $oneresult;
 	}
 
@@ -44,7 +44,7 @@ public function fetchonerecord($id)
 //Data updation Function
 public function update($name,$father,$mother,$email,$phone,$address,$userid)
 	{
-	$updaterecord=mysqli_query($this->dbh,"update  customer set name='$name',father='$father',mother='$mother',email='$email',phone='$phone',address='$address' where id='$userid' ");
+	$updaterecord=mysqli_query($this->dbh,"update  employee set name='$name',father='$father',mother='$mother',email='$email',phone='$phone',address='$address' where id='$userid' ");
 	return $updaterecord;
 	}
 
@@ -52,7 +52,7 @@ public function update($name,$father,$mother,$email,$phone,$address,$userid)
 //Data Deletion function Function
 public function delete($id)
 	{
-	$deleterecord=mysqli_query($this->dbh,"delete from customer where id=$id");
+	$deleterecord=mysqli_query($this->dbh,"delete from employee where id=$id");
 	return $deleterecord;
 	}
 
